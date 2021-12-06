1e455830dd33dd009dec471efba058edf499dc67
Morley, Jonathan <jmorley@cvent.com>
2021-12-06 13:33:23 -0500

    day6

--- src/day06.rs
+++ src/day06.rs
@@ -1,6 +1,40 @@
 use anyhow::Result;
 use aoc_runner_derive::{aoc, aoc_generator};
 
+struct LanternSchool([usize; 9]);
+
+impl From<Vec<u32>> for LanternSchool {
+  fn from(vec: Vec<u32>) -> Self {
+    LanternSchool([
+      vec.iter().filter(|x| *x == &0).count(),
+      vec.iter().filter(|x| *x == &1).count(),
+      vec.iter().filter(|x| *x == &2).count(),
+      vec.iter().filter(|x| *x == &3).count(),
+      vec.iter().filter(|x| *x == &4).count(),
+      vec.iter().filter(|x| *x == &5).count(),
+      vec.iter().filter(|x| *x == &6).count(),
+      vec.iter().filter(|x| *x == &7).count(),
+      vec.iter().filter(|x| *x == &8).count(),
+    ])
+  }
+}
+
+impl LanternSchool {
+  fn day(self) -> Self {
+    LanternSchool([
+      self.0[1],
+      self.0[2],
+      self.0[3],
+      self.0[4],
+      self.0[5],
+      self.0[6],
+      self.0[7] + self.0[0],
+      self.0[8],
+      self.0[0]
+    ])
+  }
+}
+
 #[aoc_generator(day6)]
 fn generator(input: &str) -> Result<Vec<u32>> {
     input
@@ -10,54 +44,25 @@ fn generator(input: &str) -> Result<Vec<u32>> {
         .map_err(Into::into)
 }
 
-fn lanternfish(initial: Vec<u32>, day: u32) -> Vec<u32> {
-  let mut lanternfish = initial; 
+fn lanternfish(initial: Vec<u32>, day: u32) -> usize {
+  let mut school: LanternSchool = initial.into();
 
-  for _ in 0..(day / 7) {
-    lanternfish = lanternfish_week(lanternfish);
-    dbg!(lanternfish.len());
+  for _ in 0..day {
+    school = school.day();
   }
 
-  for _ in 0..(day % 7) {
-    lanternfish = lanternfish_day(lanternfish);
-  }
-
-  lanternfish
+  school.0.into_iter().sum()
 }
 
-fn lanternfish_day(initial: Vec<u32>) -> Vec<u32> {
-  initial
-    .into_iter()
-    .flat_map(|lanternfish| if lanternfish == 0 {
-      vec![6, 8]
-    } else {
-      vec![lanternfish - 1]
-    })
-    .collect()
-}
-
-fn lanternfish_week(initial: Vec<u32>) -> Vec<u32> {
-  let new = initial
-    .clone()
-    .into_iter()
-    .map(|fish| (fish + 2))
-    .filter(|fish| fish <= &8);
-
-  initial
-    .into_iter()
-    .map(|fish| fish % 7)
-    .chain(new)
-    .collect()
-}
 
 #[aoc(day6, part1)]
 fn part1(input: &[u32]) -> usize {
-  lanternfish(input.to_owned(), 80).len()
+  lanternfish(input.to_owned(), 80)
 }
 
 #[aoc(day6, part2)]
 fn part2(input: &[u32]) -> usize {
-  lanternfish(input.to_owned(), 256).len()
+  lanternfish(input.to_owned(), 256)
 }
 
 #[cfg(test)]
@@ -69,16 +74,10 @@ mod tests {
     #[test]
     fn sample1() {
         assert_eq!(part1(&generator(SAMPLE).unwrap()), 5934);
-        assert!(false);
     }
 
     #[test]
-    fn trivial() {
-        assert_eq!(part1(&[8]), 768);
+    fn sample2() {
+        assert_eq!(part2(&generator(SAMPLE).unwrap()), 26984457539);
     }
-
-    // #[test]
-    // fn sample2() {
-    //     assert_eq!(part2(&generator(SAMPLE).unwrap()), 26984457539);
-    // }
 }
